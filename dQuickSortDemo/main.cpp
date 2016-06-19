11364d39f9306fcbb92c7d4e04558cf5b3811351
Kevin Zhang <kevinctzhang@gmail.com>
2016-06-19 15:42:12 -0700

    worst case complete best case empty

--- dQuickSortDemo/main.cpp
+++ dQuickSortDemo/main.cpp
@@ -5,25 +5,33 @@
 //  Created by Kevin Zhang on 2016-06-18.
 //  Copyright © 2016 Kevin Zhang. All rights reserved.
 //
-
-
 #include <iostream>
 #include <fstream>
 #include "dquick.hpp"
+#include <chrono>
+using namespace std::chrono;
 
 using namespace std;
 
 // you'll have to adjust this number as required
-#define ARR_LEN 50000
+//#define ARR_LEN 50000
+#define ARR_LEN 10000000
 int A[ARR_LEN];
 
 void createArraytoFile(int* A, int arrayLength);
 
 int main(int argc, const char * argv[]) {
-    //create test array here
-    for (int i =0; i<ARR_LEN ; i++) {
-        A[i] = i+1;
-    }
+    //Worst case
+//    for (int i =0; i<ARR_LEN ; i++) {
+//        A[i] = 0;
+//    }
+//
+    //Best case
+        for (int i =0; i<ARR_LEN ; i++) {
+            A[i] = i+1;
+            //pivot is as close to center as possible
+            
+        }
     
     //generate array to file
     createArraytoFile(A, ARR_LEN);
@@ -31,9 +39,11 @@ int main(int argc, const char * argv[]) {
 //    for (int i = 0; i < ARR_LEN; i++) {
 //        cin >> A[i];
 //    }
-    
+    high_resolution_clock::time_point t1 = high_resolution_clock::now();
     dquicksort(A, ARR_LEN);
-    
+    high_resolution_clock::time_point t2 = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(t2 - t1).count();
+    cout << duration << endl;
     
     //sorting check
     for (int i = 1; i < ARR_LEN; i++) {
@@ -46,15 +56,13 @@ int main(int argc, const char * argv[]) {
      }
      cout << '\n';
      */
-
-    cout << "Hello, World!\n";
     return 0;
 }
 
 void createArraytoFile(int* A, int arrayLength){
     //take the input array
     //output to text file
-    fstream outfile("worstRunningTime.txt", fstream::out);
+    fstream outfile("qs-worst.in", fstream::out);
     for (int i =0; i<arrayLength; ++i) {
         outfile << A[i] << endl;
     }
